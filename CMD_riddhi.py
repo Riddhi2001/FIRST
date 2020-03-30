a177add45f4a72787a987e3db48553f668c1d937
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-03-30 17:44:56 +0530

    Update CMD_riddhi.py

--- CMD_riddhi.py
+++ CMD_riddhi.py
@@ -1,8 +1,10 @@
 R, C =  map(int, input().split())
 a = [] 
-for i in range(0,R):
-    a.append(str(input()))
-print(a)
+for i in range(0,R):                                                        #if u input 2 2
+    a.append(str(input()))                                            # 10
+                                                                                        # 11
+print(a)    #prints (10,11)
 for j in range(0,R):
     a[j] = list(a[j])
-print(a)
+print(a)        #prints((1,0),(1,1))
+print(a[0][0])  #prints 1
