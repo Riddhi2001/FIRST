2006a6539edc1c588b8b59476772878c70e61015
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-03-31 23:44:25 +0530

    Update plants - edited by riddhi.py

--- plants - edited by riddhi.py
+++ plants - edited by riddhi.py
@@ -8,20 +8,20 @@ deaths = len(lst)
 temp = 0
 while deaths != temp:
         temp = deaths
-
-        for i in range (n) :
+        print(temp)
+        print(deaths)
+        for i in range (1,n+1-count) :
                 if lst[i] > lst[i-1]:
                         del(lst[i])
                         count += 1
                         deaths -= 1
                         n= len(lst)
                         
-                else:
-                        continue
-                
                 print(lst)
                 break
         if temp == deaths:
+                print(temp)
+                print(deaths)
                 break        
 
-print('Number of days after which no plants will die=',count-1)
+print('Number of days after which no plants will die=',count)
