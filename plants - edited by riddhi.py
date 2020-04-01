962d45eea04333beb9f421ba43b0da2b2bd90fe9
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-01 15:25:28 +0530

    Update plants - edited by riddhi.py

--- plants - edited by riddhi.py
+++ plants - edited by riddhi.py
@@ -1,27 +1,19 @@
-n=int(input())
-count=0
+n = int(input())
+plant = list(map(int,input().split()))
+count = 0
+dead_plant = []
 
-lst=[int(x) for x in input().split()]
 
-deaths = len(lst)
+for i in range(1,n):
+    if plant[i] > plant[i - 1]:
+        dead_plant.append(plant[i])
+print(dead_plant)       #prints dead plants of day 1
+for i in dead_plant: #removes dead plant from day 1
+    try: 
+        plant.remove(i) 
+    except ValueError: 
+        pass
+print(plant) #prints remaining plants of day 1
 
-temp = 0
-while deaths != temp:
-        temp = deaths
-        print(temp)
-        print(deaths)
-        for i in range (1,n+1-count) :
-                if lst[i] > lst[i-1]:
-                        del(lst[i])
-                        count += 1
-                        deaths -= 1
-                        n= len(lst)
-                        
-                print(lst)
-                break
-        if temp == deaths:
-                print(temp)
-                print(deaths)
-                break        
-
-print('Number of days after which no plants will die=',count)
+#we have to put all this in one big loop so that the remaining list of plant goes through the same process again.
+#we have to modify the code to run for until there are no more deaths.
