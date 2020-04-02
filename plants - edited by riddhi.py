fd2762bdb864aedd95d61914ef341010e4671ab5
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-02 10:38:53 +0530

    Update plants - edited by riddhi.py

--- plants - edited by riddhi.py
+++ plants - edited by riddhi.py
@@ -2,18 +2,22 @@ n = int(input())
 plant = list(map(int,input().split()))
 count = 0
 dead_plant = []
+len_p = 1
+len_d_p = 0
+while True:
+    print('1')
+    for i in range(1,n - len_d_p):
+        if plant[i] > plant[i - 1]:
+            dead_plant.append(plant[i])
 
-
-for i in range(1,n):
-    if plant[i] > plant[i - 1]:
-        dead_plant.append(plant[i])
-print(dead_plant)       #prints dead plants of day 1
-for i in dead_plant: #removes dead plant from day 1
-    try: 
-        plant.remove(i) 
-    except ValueError: 
-        pass
-print(plant) #prints remaining plants of day 1
-
-#we have to put all this in one big loop so that the remaining list of plant goes through the same process again.
-#we have to modify the code to run for until there are no more deaths.
+      
+    len_d_p = len(dead_plant)
+    print(dead_plant,len_d_p) 
+    for i in dead_plant: 
+        try: 
+            plant.remove(i) 
+        except ValueError: 
+            pass
+    len_p = len(plant)
+    print(plant,len_p)
+print('2')
