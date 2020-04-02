671152d6ac9a72b86ad2c8730c974976f36fa40e
Vicious14 <62839010+Vicious14@users.noreply.github.com>
2020-04-02 19:54:59 +0530

    Update plantsversion 2.py

--- plantsversion 2.py
+++ plantsversion 2.py
@@ -4,25 +4,30 @@ plant = list(map(int,input().split()))
 days=0
 count=0
 
+
 while x != 0:
-    death=[]
+    death =[]
     x -= 1
     temp=len(death)
     for i in range(1,n-count):
         if plant[i] > plant[i-1] :
-            death.append(plant[i])
+            death.append(i)
             count += 1
+    print(death)
 
     if len(death) == temp:
         break
     else :
         days += 1
-        for i in death:
-            plant.remove(i)
+        for i in reversed(death) :
+            #x=plant[i]
+            #plant.remove(plant[x])
+            del(plant[i])
+        print(plant)
 
-#print('Number of days after which no plants will die=',days)
-#print('Survivor plants=',plant)
-print(days)
+print('Number of days after which no plants will die=',days)
+print('Survivor plants=',plant)
+
 
 
 
