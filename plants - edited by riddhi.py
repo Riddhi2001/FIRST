6f082d87aa21e09918c34ab0b30282b160b6107b
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-02 19:37:50 +0530

    Update plants - edited by riddhi.py

--- plants - edited by riddhi.py
+++ plants - edited by riddhi.py
@@ -1,5 +1,6 @@
 n = int(input())
 plant = list(map(int,input().split()))
+plant.append(1000)
 count = 0
 dead_plant = []
 len_p = 1
@@ -7,21 +8,23 @@ len_d_p = 0
 temp = 1
 days = 0
 while True:
-    
+   
     temp = len_p
     for i in range(1,n - len_d_p):
-        if plant[i] > plant[i - 1]:
+        if plant[i] > plant[i-1]:
             dead_plant.append(plant[i])
 
       
-    len_d_p = len(dead_plant)
-     
+    len_d_p += len(dead_plant)
+    print('dead plant on day no.',days,dead_plant,len_d_p) 
 
     for i in dead_plant: 
-        try:
+        #try:
+        if i != plant[0]:
             plant.remove(i) 
-        except ValueError:
-            pass
+        #except ValueError:
+            #pass
+    dead_plant.clear()
     len_p = len(plant)
     if len_p != temp:
         days += 1
