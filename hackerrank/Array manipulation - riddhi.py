c58b3a9643c36d4860c0c4f71b945ea01335991d
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-03 18:36:16 +0530

    Update Array manipulation - riddhi.py

--- hackerrank/Array manipulation - riddhi.py
+++ hackerrank/Array manipulation - riddhi.py
@@ -9,11 +9,11 @@ for _ in range(m):
 print(queries)
 
 
-for i in range(0,3):
+for i in range(0,m):
     a = queries[i][0]
     b = queries[i][1]
     k = queries[i][2]
     for j in range(a-1,b):
-        array.remove(array[j])
-        array.insert(j,array[j]+k)
+        
+        array[j] = array[j]+k
     print(array)
