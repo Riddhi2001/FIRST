7e2154c3865e73e85fdf81b02a4c355a69af6bb1
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-05 18:01:30 +0530

    Update down to zero -riddhi.py

--- hackerrank/down to zero -riddhi.py
+++ hackerrank/down to zero -riddhi.py
@@ -5,25 +5,27 @@ for i in range(Q):
     N = int(input())
     factors=[]
     count = 0
-    while N != 0:
+    while N != 1:
         factors.clear()
-        for i in range(2,N):
+        for i in range(1,N):
             
             if N%i==0:
                 factors.append(i)
-        print(factors)
+                print(factors)
         try:    
             A = max(factors)
         except:
             pass
         print('max factor',A)
         print('original N',N)
-        if A< N-1:
+        if len(factors)==1:
+            N -= 1
+        elif A<N-1:
             N = A
-        else:
-            N -=1
+        else :
+            N -= 1
         print('new N',N)
+        #if N !=0:
         count += 1
-        
     print(count)
 
