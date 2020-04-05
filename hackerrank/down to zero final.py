77cddf249631d8d16f80094c901b601647693e6b
Vicious14 <62839010+Vicious14@users.noreply.github.com>
2020-04-05 16:52:43 +0530

    Create down to zero final.py

---
+++ hackerrank/down to zero final.py
@@ -0,0 +1,34 @@
+Q = int(input())
+A = 0
+
+for i in range(Q):
+    N = int(input())
+    factors=[]
+    count = 0
+    while N != 0:
+        factors.clear()
+        for i in range(1,N):
+            
+            if N%i==0:
+                factors.append(i)
+                print(factors)
+        try:    
+            A = max(factors)
+        except:
+            pass
+        print('max factor',A)
+        print('original N',N)
+        if len(factors)==1:
+            N -=1
+        elif A<N-1:
+            N=A
+        else :
+            N-=1
+        print('new N',N)
+        count += 1
+    print(count)
+
+
+
+
+
