5541283980cfdcaa0d3febb5f7624b6bae11333e
MsRiddhi <60326931+Riddhi2001@users.noreply.github.com>
2020-04-05 16:09:24 +0530

    Create down to zero -riddhi.py

---
+++ hackerrank/down to zero -riddhi.py
@@ -0,0 +1,19 @@
+Q = int(input())
+A = 0
+count = 1
+for i in range(Q):
+    N = int(input())
+    factors=[]
+    while N != 0:
+        for i in range(1,N):
+            if N%i==0:
+                
+                factors.append(i)
+            A = max(factors)
+        if A< N-1:
+            A = N
+        else:
+            N -=1
+        count += 1
+    print(count)
+
